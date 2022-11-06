d80b3e4b53555a40ab3c224a0b25ee8a79860aab
JotaPe <63083839+iJotape4@users.noreply.github.com>
2022-11-05 19:18:52 -0500

    Prison Scenery Imported
    Materials must be fixed
    All of them will be adjusted to URP

---
+++ Eerie/Assets/Prefabs/LevelStructure/_Scripts/ApagarLuz.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class ApagarLuz : MonoBehaviour
+{
+    /*public GameObject luces;
+    public PlayerInput _playerInput;
+    private float tiempo = 6f;
+    public NormalDialogue _eventConversation;
+    public BoxCollider _knockoutTriggerEvent;
+    public GameObject _exitDoor;
+    // Start is called before the first frame update
+
+    private void Awake()
+    {
+        _eventConversation = GetComponent<NormalDialogue>();
+    }
+
+    void Start()
+    {
+        _playerInput = FindObjectOfType<PlayerInput>();
+        _playerInput.SwitchCurrentActionMap("LimitedPlayer");
+       
+        _knockoutTriggerEvent = FindObjectOfType<KnockOutEvent>().GetComponentInChildren<BoxCollider>();
+        _exitDoor = GameObject.Find("ExitDoor");
+        _exitDoor.layer = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        tiempo -= Time.deltaTime;
+
+        if (tiempo <= 0)
+        {
+            luces.SetActive(false);
+            _eventConversation.activate();
+            _knockoutTriggerEvent.enabled = true;
+            _exitDoor.layer = 15;
+        }
+    }
+
+*/
+    
+}
