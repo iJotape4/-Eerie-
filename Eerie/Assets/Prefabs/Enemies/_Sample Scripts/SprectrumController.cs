aa0150c1fdcc54acbb4ece367851ea2574fe7082
JotaPe <63083839+iJotape4@users.noreply.github.com>
2022-11-09 12:47:32 -0500

    Imported enemies package

---
+++ Eerie/Assets/Prefabs/Enemies/_Sample Scripts/SprectrumController.cs
@@ -0,0 +1,72 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SprectrumController : MonoBehaviour
+{
+    public Transform _playerTarget;
+    public Transform _spawnPoint;
+    public GameObject _blueFire;
+    public CapsuleCollider capsuleCollider;
+    public Animator _anim;
+
+    public float shotCadency = 15f;
+    private float counter;
+
+
+    [SerializeField] private float shotForce = 15f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        _playerTarget = GameObject.FindGameObjectWithTag("Player").GetComponent<Transform>();
+        _spawnPoint = transform.GetChild(0);
+        _blueFire = Resources.Load<GameObject>("Prefabs/BlueFire");
+        capsuleCollider = GetComponent<CapsuleCollider>();
+        capsuleCollider.enabled = false;
+        _anim = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        /*if (GameManager.Instance.EerieObtained && _anim.enabled)
+        {
+            capsuleCollider.enabled = true;
+
+            counter += Time.deltaTime;
+            if (counter > shotCadency)
+            {
+                LaunchFireBall();                
+                counter = 0;          
+            }
+            if(counter > shotCadency / 2f)
+            {
+               // Move();
+            }
+
+        }*/
+
+       
+    }
+
+    public void LaunchFireBall()
+    {
+
+        transform.LookAt(_playerTarget);
+        GameObject FireBall;
+        FireBall = Instantiate(_blueFire, _spawnPoint.transform);
+        FireBall.transform.parent = null;
+        FireBall.GetComponent<Rigidbody>().AddForce(_spawnPoint.forward * shotForce);
+        FireBall.transform.LookAt(_playerTarget);
+
+    }
+
+    public void Move()
+    {
+        var position = new Vector3(Random.Range(-3.0f, 3.0f), 0, Random.Range(-3.0f, 3.0f));
+        transform.position = (Vector3.Distance(_playerTarget.position, transform.position) > 5f?
+            Vector3.MoveTowards(transform.position, _playerTarget.position, Time.deltaTime):
+            Vector3.MoveTowards(transform.position, transform.position += position, Time.deltaTime));
+        transform.LookAt(_playerTarget);
+    }
+}
---
+++ Eerie/Assets/Prefabs/Enemies/_Sample Scripts/SubBossController.cs
@@ -0,0 +1,51 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SubBossController : ZombieController
+{
+    private string _animAttackInt = "Attack";
+    // Start is called before the first frame update
+    new void Start()
+    {
+        base.Start();
+        this.gameObject.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        _anim.SetBool(_animStunnedBool, _stunned);
+
+        if (_health <= 0)
+        {
+            base.callDeath();
+        }
+       // Pursuit();
+    }
+
+ /*   protected void Pursuit()
+    {
+        if (!_player.Grounded || !_canPursuit)
+            return;
+
+        base.PursuitPlayer();
+
+        foreach (string animation in animZombieAttacks)
+        {
+            if (_anim.GetCurrentAnimatorStateInfo(0).IsName(animation))
+            {
+                //This stops the zombie to move while is attacking
+                return;
+            }
+        }
+
+        _anim.SetInteger(_animAttackInt, Random.Range(2, 5)) ;
+    }*/
+
+    protected void DisableBossHealthBar()
+    {
+       // GameManager.Instance.SubBossFight(false);
+        //UIManager.Instance.BossHealthBarActivation();
+    }
+}
