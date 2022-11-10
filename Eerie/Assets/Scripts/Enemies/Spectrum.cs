e871950659ee85cd47ff9bf80b518387b4386473
JotaPe <63083839+iJotape4@users.noreply.github.com>
2022-11-09 22:52:40 -0500

    added spectrum and maths fix

--- Eerie/Assets/Scripts/Enemies/Spectrum.cs
+++ Eerie/Assets/Scripts/Enemies/Spectrum.cs
@@ -13,15 +13,39 @@ namespace Enemies
 
         [SerializeField, Range(1f, 20f)] protected float shotCadency = 15f;
         [SerializeField] private float counter;
+        [SerializeField] private float shotForce = 15f;
 
-       protected override void Move()
-       {
+        new void Start()
+        {
+            base.Start();
+            _playerTarget = GameObject.FindGameObjectWithTag("Player").GetComponent<Transform>();
+            _spawnPoint = transform.GetChild(0);
+            _blueFire = Resources.Load<GameObject>("BlueFire");
+            capsuleCollider = GetComponent<CapsuleCollider>();
+            capsuleCollider.enabled = false;
+        }
 
-       }
+        protected override void Move()
+        {
+            var position = new Vector3(Random.Range(-3.0f, 3.0f), 0, Random.Range(-3.0f, 3.0f));
+            transform.position = (Vector3.Distance(_playerTarget.position, transform.position) > 5f?
+            Vector3.MoveTowards(transform.position, _playerTarget.position, Time.deltaTime):
+            Vector3.MoveTowards(transform.position, transform.position += position, Time.deltaTime));
+            transform.LookAt(_playerTarget);
+        }
 
-       public void SpawnAProjectile()
-       {
+        public void SpawnAProjectile()=>
+                LaunchFireBall();
+    
+        public void LaunchFireBall()
+        {
+            transform.LookAt(_playerTarget);
+            GameObject FireBall;
+            FireBall = Instantiate(_blueFire, _spawnPoint.transform);
+            FireBall.transform.parent = null;
+            FireBall.GetComponent<Rigidbody>().AddForce(_spawnPoint.forward * shotForce);
+            FireBall.transform.LookAt(_playerTarget);
 
-       }
+        }
     }
 }
